bd5db0dfd267c07a866647f96157e2074f701ad5
Caio Pereira Antunes <caioefg@gmail.com>
2023-03-07 14:55:26 -0300

    Feat: implementando função requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        pass
+        self.items = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
-
+        self.items.append(value)
+       
     def dequeue(self):
-        """Aqui irá sua implementação"""
+         if len(self.items) == 0:
+            raise IndexError("Queue is empty")
+         return self.items.pop(0)
+         
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if( 0 <= index <= len(self.items)):
+            return self.items[index]
+        else: 
+            raise IndexError("Item not found in queue")
